be5c5c1fbf7833b101b0005f3ce83ea7f560c3dd
Kamal <er.kamalsuthar@gmail.com>
2025-03-01 20:03:05 +0530

    first commit

--- src/main/java/com/platform/ticketbooking/TicketBookingApplication.java
+++ src/main/java/com/platform/ticketbooking/TicketBookingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TicketBookingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TicketBookingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TicketBookingApplication.class, args);
+    }
 
 }
