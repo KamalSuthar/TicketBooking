fa9426dd15aaba31b4c6a78298b8a416201a881c
Kamal <er.kamalsuthar@gmail.com>
2025-03-01 20:54:00 +0530

    Simple Projet

---
+++ TicketBooking/src/main/java/com/platform/ticketbooking/TicketBookingApplication.java
@@ -0,0 +1,22 @@
+package com.platform.ticketbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+		return String.format("Hello Kamal %s!", name);
+	}
+
+}
